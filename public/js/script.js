85b7d7e559c384d8f4de941a1ddc93535fce9bfa
Malpaga <philippemangeard@gmail.com>
2020-06-05 17:51:12 +0200

    oui

--- public/js/script.js
+++ public/js/script.js
@@ -1,9 +1,20 @@
 
+function activate()
+{
+    hamburger = document.getElementById('burg');
+    if(hamburger.classList.contains('is-active')){
+        document.getElementById('nl').style.height = document.getElementById('')
+        document.getElementById('nl').style.transform = "translateX(-100%)";
+        hamburger.classList.remove('is-active');
+    
+    }else{
+        hamburger.classList.add('is-active');
+        document.getElementById('nl').style.transform = "translateX(0)";
+    }
+}
+
 window.onscroll = function()
         {
-            if(document.documentElement.scrollTop > 50){
-                document.querySelector('header').className = "menu fixed";
-            }else{
-                document.querySelector('header').className = "menu";
-            }
+            header = document.getElementById('header');
+            header.style.backgroundColor = "rgba(48, 51, 107, 0.8)";
         }
